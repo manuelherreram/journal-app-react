99d47c30e248ea7ceaffe4899e747ecbb0950175
Manuel <manuel.herrera.m@gmail.com>
2024-10-02 13:30:37 -0300

    Material UI añadido

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
 
 export const AppRouter = () => {
-  return;
-
-  <Routes>
-    <Route />
-    <Route />
-  </Routes>;
+  return (
+    <Routes>
+      <Route path="auth/*" element={<AuthRoutes />} />
+      <Route path="/*" element={<JournalRoutes />} />
+      <Route />
+    </Routes>
+  );
 };
