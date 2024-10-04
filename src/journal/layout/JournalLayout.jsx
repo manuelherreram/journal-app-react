cfb94c98f3246f698240155396ac7feaa3a4701f
Manuel <manuel.herrera.m@gmail.com>
2024-10-04 11:42:58 -0300

    Sidebar añadido

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,14 +1,16 @@
-import { Box } from "@mui/material";
-import { NavBar } from "../components";
+import { Box, Toolbar } from "@mui/material";
+import { NavBar, SideBar } from "../components";
 
-const drawerWith = 240;
+const drawerWith = 280;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
       <NavBar drawerWith={drawerWith} />
+      <SideBar drawerWith={drawerWith} />
 
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
         {children}
       </Box>
     </Box>
