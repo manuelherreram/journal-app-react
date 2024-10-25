33dc7271be85865dec5eaa56e9046c05338ed8b7
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-10-24 22:48:29 -0300

    isSaving e isActive añadidos al thunks

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,8 +1,10 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
+import { addNewEmptyNote, savingNewNote, setActiveNote } from "./journalSlice";
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
+    dispatch(savingNewNote());
     const { uid } = getState().auth;
     //uid
 
@@ -15,8 +17,11 @@ export const startNewNote = () => {
     const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
     await setDoc(newDoc, newNote);
 
+    newNote.id = newDoc.id;
+
     // dispatch
-    //dispatch (newNote)
+    dispatch(addNewEmptyNote(newNote));
+    dispatch(setActiveNote(newNote));
     // dispatch(activarNote)
   };
 };
